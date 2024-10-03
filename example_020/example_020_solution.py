2b8794599ba48b2c0398405ac7fc7d9ebddce01e
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-03 14:19:29 +0300

    Update example_020_solution.py

---
+++ example_020/example_020_solution.py
@@ -0,0 +1,3 @@
+metin = input("bir metin giriniz: ") #kullanıcıdan metin al
+yeni_metin =metin.title()            #her kelimenin ilk harfini büyük yapan fonksiyon
+print(yeni_metin)                    # yazdır
