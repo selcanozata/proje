404beb669d15748bc4baba411fbd9475bd829a3e
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-03 17:02:53 +0300

    Update example_027_solution.py

---
+++ example_027/example_027_solution.py
@@ -0,0 +1,5 @@
+dizi = [3,5,74,12] #kullanıcıdan bir dizi al
+toplam = sum(dizi) #dizinin toplamını bulur
+ortalama= toplam / len(dizi) #dizinin ortalaması bulunur
+print("Toplam:", toplam)
+print("Ortalama: ", ortalama)
