4bcfe52fd26cfea4c156b9f895279c2beec4e3c5
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-03 13:44:58 +0300

    Update example_018_solution.py

---
+++ example_018/example_018_solution.py
@@ -0,0 +1,7 @@
+sayi = float(input("bir sayı girin: ")) # kullanıcıdan bir sayı alınız
+if sayi > 0:    # 0 dan büyük ise
+    print("pozitif")
+elif sayi<0:    # 0 dan küçük ise
+    print("negatif")
+else:            # 0 ise 
+    print("sıfır")
