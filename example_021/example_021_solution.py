b7fe1b9995ea85c3f06038349ecf4b19c89a48a6
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-03 14:29:32 +0300

    Update example_021_solution.py

---
+++ example_021/example_021_solution.py
@@ -0,0 +1,4 @@
+liste=[1,8,17,56,14] # listeyi tanımla
+toplam = sum(liste)  # sum() fonksiyonu listeyi toplar
+ortalama = toplam / len(liste) # listenin ortalaması hesaplanır
+print(f"Toplam: {toplam}, Ortalama: {ortalama}") # yazdır
