88381f7df0dda802ac906d82608b745b8dd5e290
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-04 11:49:51 +0300

    Update example_031_solution.py

---
+++ example_031/example_031_solution.py
@@ -0,0 +1,6 @@
+dizi = [45, 12, 8, 4]
+toplam = 0
+for sayi in dizi:
+    if sayi % 2 == 0:  # Sayının çift olup olmadığını kontrol et
+        toplam += sayi
+print("Çift sayıların toplamı:", toplam)
