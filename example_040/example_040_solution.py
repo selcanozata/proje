50a8cd01234fa70e81c2e515e4ca8307f04e3bca
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-04 14:02:07 +0300

    Update example_040_solution.py

---
+++ example_040/example_040_solution.py
@@ -0,0 +1,5 @@
+sayi = int(input("Bir sayı girin: ")) # kullanıcıdan bir sayı al
+if sayi % 5 == 0:  
+    print("Evet, bu sayı 5 ile tam bölünüyor.")
+else:
+    print("Hayır, bu sayı 5 ile tam bölünmüyor.")
