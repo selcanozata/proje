ebddf279482da43fb803d7a29c394a197315d8f8
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-04 12:12:14 +0300

    Update example_033_solution.py

---
+++ example_033/example_033_solution.py
@@ -0,0 +1,3 @@
+dizi =[4,7,9,15] #diziyi oluştur
+kareler_listesi = [sayı**2 for sayı in dizi] # her sayının karesi alınır yeni bir liste oluşturulur
+print("Kareleri alınmış yeni liste: ", kareler_listesi) #yazdır
