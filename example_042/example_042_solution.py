d13f3568ad0a708f5d1e4199bdf8d6cfe64b5662
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-04 14:37:18 +0300

    Update example_042_solution.py

---
+++ example_042/example_042_solution.py
@@ -0,0 +1,5 @@
+kelime = input("Bir kelime giriniz: ") #kullanıcıdn bir kelime alınız
+if "e" in kelime or "E" in kelime:   # e olup olmadığını kontrol etme
+    print("Var")
+else:
+    print("Yok")
