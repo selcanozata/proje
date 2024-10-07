3e533e691edfa04019415af49175fed337e7005a
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-07 16:46:32 +0300

    Update example_058_solution.py

---
+++ example_058/example_058_solution.py
@@ -0,0 +1,3 @@
+metin = input("Bir metin giriniz: ") #kullanıcıdan metin al
+kucuk_harf_metin =metin.lower() # metindeki tüm büyük harfleri küçük harfe dönüştürür
+print(f"Küçük harfli metin: {kucuk_harf_metin}") 
