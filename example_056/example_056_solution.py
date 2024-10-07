c76fba41fdb4bde3ad739f7f84064697fee7dd87
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-07 15:57:52 +0300

    Update example_056_solution.py

---
+++ example_056/example_056_solution.py
@@ -0,0 +1,7 @@
+sayi= float(input("Bir sayı giriniz: ")) # kullanıcıdan bir sayı al
+if sayi > 0:  # sayının pozitif veya negatif olduğunu kontrol et
+    print("Pozitif")
+elif sayi < 0:
+    print("Negatif")
+else:
+    print("Sıfır")   
