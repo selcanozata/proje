9ff0b0c9fafb5f939d9e83f8fa371eb3a82f6f6e
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-08 12:24:59 +0300

    Update example_063_solution.py

---
+++ example_063/example_063_solution.py
@@ -0,0 +1,6 @@
+
+sayi = int(input("Bir sayı girin: ")) #kullanıcıdan bir sayı alın
+karesi = sayi ** 2 #karesi
+kup = sayi ** 3 #küpü
+print(f"{sayi} sayısının karesi: {karesi}")
+print(f"{sayi} sayısının küpü: {kup}")    
