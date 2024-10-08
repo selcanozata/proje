9b9183d500599ed8aaf9a5a48f57bf737fbede87
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-08 12:21:03 +0300

    Update example_070_solution.py

---
+++ example_070/example_070_solution.py
@@ -0,0 +1,3 @@
+metin = input("Bir metin girin: ") #kullanıcıdan bir metin alınız
+yeni_metin = metin.replace('a', '') #a harfini kaldırma replace foksiyonu ile
+print(f"'a' harfi çıkarılmış metin: {yeni_metin}")
