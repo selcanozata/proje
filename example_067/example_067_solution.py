ee149e26a060cb307ae217c78411a73d1e77a41c
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-08 12:22:57 +0300

    Update example_067_solution.py

---
+++ example_067/example_067_solution.py
@@ -0,0 +1,5 @@
+sayi = int(input("Bir sayı girin: ")) #kullanıcıdan sayı alma
+if sayi % 2 == 0: #2 nin katı olup olmadığını kontrol etme
+    print("Evet, bu sayı 2'nin katıdır.")
+else:
+    print("Hayır, bu sayı 2'nin katı değildir.")   
