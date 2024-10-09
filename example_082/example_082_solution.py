ba5f0d82d8fbb8e30867c639216ad99b1b48f6b6
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-09 13:11:47 +0300

    Update example_082_solution.py

---
+++ example_082/example_082_solution.py
@@ -0,0 +1,5 @@
+sayı = int(input("Bir sayı girin: ")) # kullanıcıdan bir sayı al
+karesi = sayı**2 #sayının karesini hesapla
+küpü = sayı**3 #sayının küpünü hesapla
+print("Sayının karesi: ", karesi) #sonucu ekrana yazdır
+print("Sayının küpü: ", küpü)
