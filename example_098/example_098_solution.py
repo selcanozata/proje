583fc94f2c6bd2a0eb9525628d2bb129be7abc5a
Selcan ÖZATA <96137446+selcanozata@users.noreply.github.com>
2024-10-11 13:10:33 +0300

    Update example_098_solution.py

---
+++ example_098/example_098_solution.py
@@ -0,0 +1,6 @@
+
+sayi = int(input("Bir sayı girin: "))
+if sayi % 3 == 0: # sayının 3 ile bölünmesini kontrol etme
+    print("Evet, sayı 3 ile tam bölünüyor.")
+else:
+    print("Hayır, sayı 3 ile tam bölünmüyor.")  
